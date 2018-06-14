40963ebc3df83db30a0a4f0a50c9b95a743e11fc
Serhii Zharyi <zharyi.serhii@gmail.com>
2018-06-14 14:40:24 +0300

    initial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+;(function($) {
+	"use strict";
+
+	// var slider = $('.ba-slider').slick();
+
+	var slider = $('.ba-slider');
+	slider.slick({
+        dots: true,
+        infinite: true
+	});
+
+})(jQuery);
